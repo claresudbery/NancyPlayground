848dd71e8ac063b48c2d207add550db045f850fc
Clare Sudbery <clare.sudbery@nice.org.uk>
2016-04-08 17:48:48 +0100

    Added RatesQueryModule plus sample RatesQuery error xml

---
+++ NancyPlayground/Modules/RatesQueryModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+
+namespace NancyPlayground.Modules
+{
+    public class RatesQueryModule : NancyModule
+    {
+        public RatesQueryModule()
+        {
+            Get["/api/htng2014b"] = parameters => "Hello from Rates Query";
+            Post["/api/htng2014b"] = parameters => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            + "\n<OTA_ErrorRS TimeStamp=\"2016-04-08T12:09:17.641Z\""
+            + "\nVersion=\"3.000\""
+            + "\nxmlns=\"http://www.opentravel.org/OTA/2003/05\""
+            + "\nErrorCode=\"448xx\""
+            + "\nErrorMessage=\"Request could not be processed due to an internal error.\""
+            + "\nStatus=\"Unknown\""
+            + "\nCorrelationID=\"a0c0cc00-73c0-49c0-a5c5-32288f32f29a\"/>";
+        }
+    }
+}
